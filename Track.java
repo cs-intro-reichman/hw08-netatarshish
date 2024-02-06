bf4951ec0635903965162138f0ef9b03e2f944f4
Neta Tarshish <neta.tarshish@runi.ac.il>
2024-02-06 21:46:50 +0200

    HW8

---
+++ Track.java
@@ -0,0 +1,58 @@
+/** Represents a music track. A track has a title (String), an artist (String), 
+ *  and a duration (int), in seconds. */
+class Track {
+    private String title;
+    private String artist;
+    private int duration;
+
+    /** Constructs a track from the given values. */
+    public Track(String title, String artist, int duration) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+
+    /** Returns this track's data as "artist, title, minutes:seconds".
+     *  For example, "John Lennon, Imagine, 3:07" */
+    public String toString() {
+        int minutes = (int)(duration/60);
+        int seconds = duration%60;
+        String newSeconds = seconds;
+        if(seconds<10){
+            newSeconds = "0" + String.valueOf(seconds);
+        }
+        return artist + ", " + title + ", " + minutes + ":" + newSeconds;
+    }
+
+    /** Returns this track's title. */
+    public String getTitle() {
+        return title;
+    }
+    /** Returns this track's artist. */
+    public String getArtist() {
+        return artist;
+    }
+    /** Returns this track's duration. */
+    public int getDuration() {
+        return duration;
+    }
+
+    /** If this track's duration is shorter than the other track's duration
+     *  returns true; otherwise returns false. */
+    public boolean isShorterThan(Track other) {
+        return duration < other.duration;
+    }
+
+    // Returns a string that represents the totalSeconds as "minutes:seconds",
+    // Where seconds is always two digits. For example, "3:17" or "12:05".
+    private String formattedDuration(int totalSeconds) {
+        int minutes = (int)(Track.totalDuration()/60);
+        int seconds = Track.totalDuration()%60;
+        String newSeconds = seconds;
+        if(seconds<10){
+            newSeconds = "0" + String.valueOf(seconds);
+        }
+
+        return minutes + ":" + newSeconds;
+    }
+}
